5ab9af850b7eb30ee4e390ff8502b622e7fd4b0f
Sebastian Bejga <sebastian.bejga@iteratec.com>
2021-04-14 21:06:49 +0200

    rough office

---
+++ tilesets/modern_interiors_ukg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="modern_interiors_ukg" tilewidth="32" tileheight="32" tilecount="8082" columns="18">
+ <image source="../../gparant_tcm-client/TCM/crea/assets/modern_interiors_ukg.png" width="600" height="14368"/>
+</tileset>
