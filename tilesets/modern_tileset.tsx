39e813cd24083ef27f5e34d8d7c1ea1c9ffeacf9
Sebastian Bejga <sebastian.bejga@iteratec.com>
2021-04-14 21:12:08 +0200

    tilesets embedded

--- tilesets/modern_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="modern_tileset" tilewidth="32" tileheight="32" tilecount="2320" columns="40">
- <image source="../../gparant_tcm-client/TCM/crea/assets/modern_tileset.png" width="1280" height="1856"/>
-</tileset>
--- tilesets/office_interiors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="office_interiors" tilewidth="32" tileheight="32" tilecount="528" columns="22">
- <image source="../../gparant_tcm-client/TCM/crea/assets/office_interiors.png" width="704" height="768"/>
-</tileset>
